6248d3fc6a44cf0e82153de3f76f3585b5ed84b2
Dragon|Rider <draco.draconix@gmail.com>
2020-02-04 23:17:28 +0100

    initial commit

---
+++ src/main/java/pl/workshop/stermedia/ai/demo/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package pl.workshop.stermedia.ai.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(@RequestParam Optional<String> name) {
+        return name.map(n -> "Hello " + n + "!").orElse("Hello, World!");
+    }
+}
